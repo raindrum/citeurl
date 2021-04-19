4bdc4ca0e84f989e0fa7e57e42eb52a35ad430ed
raindrum <simonraindrum@gmail.com>
2021-04-18 19:53:59 -0700

    various regex tweaks

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='citeurl',
-    version='6.0.0',
+    version='6.0.1',
     description='an extensible tool to process legal citations in text',
     author='Simon Raindrum Sherred',
     author_email='simonraindrum@gmail.com',
