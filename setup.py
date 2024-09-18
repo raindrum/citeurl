1c86f03f3a171d097fd99e3eae5c042f5eac72a0
raindrum <=>
2024-09-18 01:48:46 -0700

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name = 'citeurl',
-    version = '11.4.2',
+    version = '11.4.3',
     description = 'an extensible tool to process legal citations in text',
     author = 'Simon Raindrum Sherred',
     author_email = 'simonraindrum@gmail.com',
