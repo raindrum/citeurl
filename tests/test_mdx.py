a929a29d7b72b5bf376d90f48759a5496ecaf799
raindrum <=>
2024-09-27 02:18:12 -0700

    account for new CAP url format

--- tests/test_mdx.py
+++ tests/test_mdx.py
@@ -5,7 +5,7 @@ from markdown import markdown
 def test_default_mdx():
     "make sure the default markdown extension works"
     output = markdown('413 F. Supp. 1281', extensions=['citeurl'])
-    assert output == '<p><a class="citation" href="https://cite.case.law/f-supp/413/1281" title="413 F. Supp. 1281">413 F. Supp. 1281</a></p>'
+    assert output == '<p><a class="citation" href="https://case.law/caselaw/?reporter=f-supp&volume=413&case=1281-01" title="413 F. Supp. 1281">413 F. Supp. 1281</a></p>'
 
 def test_configured_mdx():
     "make sure at least a few of the markdown config options work"
@@ -14,4 +14,4 @@ def test_configured_mdx():
         extensions=['citeurl'],
         extension_configs={'citeurl': {'attributes': {'class': 'cite'}}}
     )
-    assert output == '<p><a class="cite" href="https://cite.case.law/f-supp/413/1281" title="413 F. Supp. 1281">413 F. Supp. 1281</a></p>'
+    assert output == '<p><a class="cite" href="https://case.law/caselaw/?reporter=f-supp&volume=413&case=1281-01" title="413 F. Supp. 1281">413 F. Supp. 1281</a></p>'
