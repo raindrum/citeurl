b87d2f12c117e72d696d4bf7ffa0722f907eab81
raindrum <=>
2026-01-19 18:59:31 -0800

    allow reference to "citeurl.makeExtension" to fix import error in testing environment

--- citeurl/__init__.py
+++ citeurl/__init__.py
@@ -2,3 +2,4 @@ from .citation import Citation
 from .citator import Citator, Template, cite, list_cites, insert_links
 from .authority import Authority, list_authorities
 from .tokens import TokenType, TokenOperation, StringBuilder
+from .mdx import makeExtension
